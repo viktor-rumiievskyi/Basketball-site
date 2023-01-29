d6a4748fc7682fd0febd7e747aa0e4e99197fe32
viktor-rumiievskyi <vityok05042002@gmail.com>
2023-01-29 13:55:32 +0200

    Finish

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function buttonUp() {
+	let button  = $ ('.button-up');
+
+	$(window).on('scroll', () =>  {
+		if ($(this).scrollTop() >= 50) {
+			button.fadeIn();
+		} else {
+			button.fadeOut();
+		}
+	});
+
+	button.on('click', (e) => {
+		e.preventDefault();
+		$('html').animate({scrollTop: 0}, 1000);
+	})
+}
+
+buttonUp();
